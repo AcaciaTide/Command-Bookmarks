9969696e98e00a38ae4a197811796dde28c6f665
AcaciaTide <252184054+AcaciaTide@users.noreply.github.com>
2026-03-06 08:31:20 +0900

    chore: 初回コミット

---
+++ src/client/java/acaciatide/commandbookmark/CommandbookmarkClient.java
@@ -0,0 +1,10 @@
+package acaciatide.commandbookmark;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CommandbookmarkClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
